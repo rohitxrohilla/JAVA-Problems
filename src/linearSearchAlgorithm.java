77478484d2de2a747834687cf2d08ede757c68b2
rohitxrohilla <rohitxrohilla@gmail.com>
2025-05-10 16:42:09 +0530

    more practice

---
+++ src/linearSearchAlgorithm.java
@@ -0,0 +1,77 @@
+public class linearSearchAlgorithm {
+    public static void main(String[] args) {
+
+////       1. to check 14 exists or not
+//        int[] A = {12,33,44,90,67,14,47};
+//        int key = 14;
+//
+//        for (int i=0;i<A.length;i++){
+//            if (key==A[i]){
+//                System.out.println("14 exists at " + i);
+//                System.exit(0);
+//            }
+//        }
+//        System.out.println("not found");
+
+////2. to check character exists or not (string)
+//
+//        String str = "panchkula";
+//        char key = 'k';
+//
+//        for (int i=0;i<str.length();i++){
+//            if (key == str.charAt(i)){
+//                System.out.println(" k exists at " + i);
+//                System.exit(0);
+//            }
+//        }
+//        System.out.println("not found");
+
+//// 3.Search for 3 in the range of index[1,4]
+//
+//        int[] A = {18,12,-7,3,14,28};
+//        int key = 3;
+//
+//        for (int i=1;i<A.length-1;i++){
+//            if (key==A[i]){
+//                System.out.println(i);
+//                System.exit(0);
+//            }
+//        }
+//        System.out.println("not found");
+
+
+//// 4. min number in array
+//
+//        int [] A = {10,14,3,7,1,9};
+//        int min = A[0];
+//
+//        for ( int i = 0;i<A.length;i++){
+//            if (min>A[i]){
+//                min = A[i];
+//            }
+//        }
+//        System.out.println(min);
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/methods/intro.java
+++ src/methods/intro.java
@@ -1,7 +1,43 @@
 package methods;
 
+import java.util.Arrays;
+
 public class intro {
-    public static void main(String[] args) {
+
+//    static int max(int x,int y){
+//        if (x>y)
+//            return x;
+//        else return y;
+//    }
+//
+//    public static void main(String[] args) {
+//        int a = 10; int b = 15; int c;
+//        c = max(a,b);
+//       System.out.println(c);
+//    }
+
+
+//    static void inc(int x){
+//        x++;
+//        System.out.println(x);
+//    }
+//
+//    public static void main(String[] args) {
+//        int a = 10;
+//        inc(a);
+//        System.out.println(a);
+//    }
+
+    static void update(int[] A){
+        A[0]=25;
 
     }
+
+    public static void main(String[] args) {
+        int [] A = {2,4,4,5};
+        update(A);
+        System.out.println(Arrays.toString(A));
+    }
+
+ 
 }
